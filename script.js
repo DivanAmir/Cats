7ab52ee6690f5b05a9c437438d38a2ceac4ae542
DivanAmir <93771275+DivanAmir@users.noreply.github.com>
2022-09-04 13:06:11 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+let header = document.getElementById("header")
+function checkScroll(){
+if(header.scrollTop > 200){
+header.style.backgroundColor = "white"
+header.style.color = "black"
+console.log("hello");
+}
+}
